3ead1420eb9b7a1d5acb2df61780c00e5817af68
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-04-24 20:36:11 +0530

    done

--- src/app/(Main)/(Routes)/(overview)/_components/ProductPage.tsx
+++ src/app/(Main)/(Routes)/(overview)/_components/ProductPage.tsx
@@ -31,7 +31,7 @@ const productImages = [
     url: "https://images.unsplash.com/photo-1601233216647-4fb22eb08425?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2xpcHBlcnN8ZW58MHx8MHx8fDA%3D",
   },
   {
-    id: 2,
+    id: 5,
     url: "https://images.unsplash.com/photo-1622920799137-86c891159e44?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2xpcHBlcnN8ZW58MHx8MHx8fDA%3D",
   },
 ];
