1c2f2499aa9b38d4ee8111b6a4d32c99cd884e57
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-04-24 20:14:15 +0530

    image

--- src/app/(Main)/(Routes)/(overview)/_components/ProductPage.tsx
+++ src/app/(Main)/(Routes)/(overview)/_components/ProductPage.tsx
@@ -16,8 +16,7 @@ import React, { useState } from "react";
 const productImages = [
   {
     id: 1,
-    url: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8fDA%3D",
-  },
+    url: "https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c2hvZXN8ZW58MHx8MHx8fDA%3D"
   {
     id: 2,
     url: "https://images.unsplash.com/photo-1622920799137-86c891159e44?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2xpcHBlcnN8ZW58MHx8MHx8fDA%3D",
